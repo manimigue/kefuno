3051aa9ac25802462b3a02a1564c87363c2bae27
Travis CI <travis@travis-ci.org>
2020-09-19 08:16:18 +0000

    Travis update: Sep 2020 (Build 23)
    [skip ci]

---
+++ src/news/articles/news2_20200919.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import MarkdownRender from "@nteract/markdown";
+import replace_assets from '../../article/replace_assets';
+import I0 from './assets/homepic.png'
+const paths = {
+  'homepic.png' : I0
+}
+
+export default () => {
+  const [markdown, setMarkdown] = useState('loading...')
+
+  useEffect(() => {
+    const path = require("../markdown/デモ2.md");
+
+    fetch(path)
+    .then(response => {
+      return response.text()
+    })
+    .then(text => {    
+      setMarkdown(replace_assets(text,paths))
+    })
+  })
+  return (
+    <MarkdownRender 
+      className='article' 
+      source={markdown} 
+      escapeHtml={false}
+    />
+  )
+}
--- src/news/component.jsx
+++ src/news/component.jsx
@@ -1,5 +1,7 @@
 import demo1 from './articles/demo1'
+import news2_20200919 from './articles/news2_20200919'
 
 export default [
-  { url: 'demo1', component: demo1 }
+  { url: 'demo1', component: demo1 },
+  { url: 'news2_20200919', component: news2_20200919 }
 ]
