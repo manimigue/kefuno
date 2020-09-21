460f2f84cd97c2a4af306a571481a750ff1be230
manimigue <manimigue0701@gmail.com>
2020-09-22 03:23:56 +0900

    [FIX]Change news id=7 to news7

--- src/news/component.jsx
+++ src/news/component.jsx
@@ -3,14 +3,14 @@ import news6_20200921 from './articles/news6_20200921'
 import news3_20200919 from './articles/news3_20200919'
 import news5_20200920 from './articles/news5_20200920'
 import news2_20200919 from './articles/news2_20200919'
-import news5_20200921 from './articles/news5_20200921'
+import news7_20200921 from './articles/news7_20200921'
 import demo1 from './articles/demo1'
 
 export default [
   { url: 'news4_20200918demo4', component: news4_20200918demo4 },
   { url: 'news2_20200919', component: news2_20200919 },
   { url: 'news6_20200921', component: news6_20200921 },
-  { url: 'news5_20200921', component: news5_20200921 },
+  { url: 'news7_20200921', component: news7_20200921 },
   { url: 'news3_20200919', component: news3_20200919 },
   { url: 'news5_20200920', component: news5_20200920 },
   { url: 'demo1', component: demo1 }
