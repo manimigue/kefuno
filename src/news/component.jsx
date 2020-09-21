c7940eb7c8d808c73a8eff1e9d517f5979cbf127
Travis CI <travis@travis-ci.org>
2020-09-21 18:29:19 +0000

    Travis update: Sep 2020 (Build 34)
    [skip ci]

--- src/news/component.jsx
+++ src/news/component.jsx
@@ -1,17 +1,17 @@
+import demo1 from './articles/demo1'
 import news4_20200918demo4 from './articles/news4_20200918demo4'
 import news6_20200921 from './articles/news6_20200921'
 import news3_20200919 from './articles/news3_20200919'
+import news7_20200921 from './articles/news7_20200921'
 import news5_20200920 from './articles/news5_20200920'
 import news2_20200919 from './articles/news2_20200919'
-import news7_20200921 from './articles/news7_20200921'
-import demo1 from './articles/demo1'
 
 export default [
   { url: 'demo1', component: demo1 },
   { url: 'news4_20200918demo4', component: news4_20200918demo4 },
   { url: 'news6_20200921', component: news6_20200921 },
-  { url: 'news7_20200921', component: news7_20200921 },
   { url: 'news3_20200919', component: news3_20200919 },
+  { url: 'news7_20200921', component: news7_20200921 },
   { url: 'news5_20200920', component: news5_20200920 },
   { url: 'news2_20200919', component: news2_20200919 }
 ]
