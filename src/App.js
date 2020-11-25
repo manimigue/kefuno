6edf0e653a0ec47b8fd58b03a5f1485fc0dc4c53
manimigue <manimigue0701@gmail.com>
2020-11-25 22:06:29 +0900

    [ADD] google fonts, noto and sorts mill

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Footer from './containers/Footer'
 import NewsRoutes from './news'
 import Home from './containers/Home'
 import Header from './containers/Header';
-import { delHome, selectHome } from './store/news'
+// import { delHome, selectHome } from './store/news'
 
-const News =  lazy(() => import( './containers/News_api'))
+const News =  lazy(() => import( './containers/News'))
 const Contact =  lazy(() => import( './containers/Contact'));
 const Tickets =  lazy(() => import( './containers/Ticket'));
-const Article = lazy(() => import('./containers/Article'))
+// const Article = lazy(() => import('./containers/Article'))
 
 const renderLoader = () => <p>Loading</p>;
 
@@ -22,10 +22,11 @@ function App({ strapiRoot }) {
       <div className='main'>
         <Route exact path='/' component={Home} />
         <Suspense fallback={renderLoader()}>
-          <Route exact path='/news' component={() => <News root={strapiRoot} />} />
+          {/* <Route exact path='/news' component={() => <News root={strapiRoot} />} /> */}
+          <Route exact path='/news' component={News} />
           <Route exact path='/ticket' component={Tickets} />
           <Route exact path='/contact' component={Contact} />
-          <Route path='/news/:url' component={() => <Article type="news" root={strapiRoot} delHome={delHome} selectHome={selectHome}/>} />
+          {/* <Route path='/news/:url' component={() => <Article type="news" root={strapiRoot} delHome={delHome} selectHome={selectHome}/>} /> */}
         </Suspense>
         <NewsRoutes />
       </div>
