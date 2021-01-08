830fd5e3e52cadbc6d48146ad18c4b7c60ffddad
manimigue <manimigue0701@gmail.com>
2021-01-08 09:36:04 +0900

    [FIX] contact new system

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,22 +1,53 @@
 import React,{ Component } from 'react';
-import {Helmet} from 'react-helmet';
+import Helmet from 'react-helmet'
 
 import '../sass/main/contact.scss'
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
   componentDidMount() {
     window.scrollTo(0, 0)
   }
 
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+
   render(){
-    const mail = "kefuno2019@gmail.com";
+    const success = this.state.status === "SUCCESS"
     return (
       <div className="contact-form">
         <Helmet>
           <title>kefuno. お問い合わせ</title>
         </Helmet>
         <h2 className="title">Contact</h2>
-        <form method="POST" action={"https://formspree.io/" + mail} >
+        <form 
+          onSubmit={this.submitForm}
+          action="https://formspree.io/f/xknpkrrp"
+          method="POST"
+        >
           <p>お名前</p>
           <input type='text' name='お名前' placeholder="佐藤　太郎" />
           <p>メールアドレス</p>
@@ -25,7 +56,7 @@ class Contact extends Component {
             <input type='text' name='件名' />
           <p>お問い合わせ内容</p>
           <textarea name='お問い合わせ内容'></textarea>
-          <input className='submit' type="submit" value='Send'/>
+          <input className={success ? 'submit success' : 'submit'} type="submit" value={success ? 'Thank you!' : 'Send'}/>
         </form>
       </div>
     )
