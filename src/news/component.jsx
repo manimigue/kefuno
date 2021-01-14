0a6d99b3a6a840e7e4ff332e35d619d38530d3cf
Travis CI <travis@travis-ci.org>
2021-01-14 08:38:52 +0000

    Travis update: Jan 2021 (Build 79)
    [skip ci]

--- src/news/component.jsx
+++ src/news/component.jsx
@@ -1,6 +1,8 @@
+import news2_20210114 from './articles/news2_20210114'
 import news1_20210107 from './articles/news1_20210107'
 
 const newsComponents = [
+  { url: 'news2_20210114', component: news2_20210114 },
   { url: 'news1_20210107', component: news1_20210107 }
 ]
 export default newsComponents
