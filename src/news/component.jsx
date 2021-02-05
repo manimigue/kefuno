516a5cc3e90c5d70955ccac453838af953a64714
Travis CI <travis@travis-ci.org>
2021-02-05 11:48:46 +0000

    Travis update: Feb 2021 (Build 80)
    [skip ci]

--- src/news/component.jsx
+++ src/news/component.jsx
@@ -1,7 +1,9 @@
+import news3_20210205 from './articles/news3_20210205'
 import news2_20210114 from './articles/news2_20210114'
 import news1_20210107 from './articles/news1_20210107'
 
 const newsComponents = [
+  { url: 'news3_20210205', component: news3_20210205 },
   { url: 'news2_20210114', component: news2_20210114 },
   { url: 'news1_20210107', component: news1_20210107 }
 ]
