8fdcf809f6626f767841ad2908ccd28b0fc3f032
Travis CI <travis@travis-ci.org>
2021-04-12 02:05:03 +0000

    Travis update: Apr 2021 (Build 87)
    [skip ci]

--- src/news/component.jsx
+++ src/news/component.jsx
@@ -4,6 +4,7 @@ import news2_20210114 from './articles/news2_20210114'
 import news6_20210313 from './articles/news6_20210313'
 import news4_20210205 from './articles/news4_20210205'
 import news7_20210327 from './articles/news7_20210327'
+import news8_20210412 from './articles/news8_20210412'
 import news1_20210107 from './articles/news1_20210107'
 
 const newsComponents = [
@@ -13,6 +14,7 @@ const newsComponents = [
   { url: 'news6_20210313', component: news6_20210313 },
   { url: 'news4_20210205', component: news4_20210205 },
   { url: 'news7_20210327', component: news7_20210327 },
+  { url: 'news8_20210412', component: news8_20210412 },
   { url: 'news1_20210107', component: news1_20210107 }
 ]
 export default newsComponents
